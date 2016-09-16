264cd4475fe6a9b16ab2389c82f249b3fa0d7683
bex3 <Rebeccam.bearden@gmail>
2016-09-16 11:38:34 -0400

    need unit test

--- src/main/java/tiy/webapp/SampleSpringAppController.java
+++ src/main/java/tiy/webapp/SampleSpringAppController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class SampleSpringAppController {
+    WebChatClient myWebChatClient;
+
     @RequestMapping(path = "/person-url", method = RequestMethod.GET) //maps to URL
     public String person(Model model, String name, String city, int age) { //this person name doesn't matter
         Person p = new Person(name, city, age);
@@ -21,15 +23,33 @@ public class SampleSpringAppController {
         model.addAttribute("name", session.getAttribute("userName"));
         return "home";
     }
+
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(HttpSession session, String userName) {
         session.setAttribute("userName", userName);
         return "redirect:/";
     }
+
     @RequestMapping(path = "/logout", method = RequestMethod.GET)
     public String logout(Model model, HttpSession session) {
         session.removeAttribute("userName");
         return "redirect:/";
     }
 
+    @RequestMapping(path = "/entermessage", method = RequestMethod.POST)
+    public String entermessage (HttpSession session, String message) {
+        session.removeAttribute("message");
+        myWebChatClient = new WebChatClient();
+        myWebChatClient.SendMessage(message);
+
+
+        return"redirect:/chat";
+    }
+
+
+    @RequestMapping (path = "/chat", method = RequestMethod.GET)
+    public String chat(Model model, HttpSession session, String message){
+
+        return "chat";
+    }
 }
--- src/main/java/tiy/webapp/WebChatClient.java
+++ src/main/java/tiy/webapp/WebChatClient.java
@@ -1,19 +1,42 @@
 package tiy.webapp;
 
+
 import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /**
  * Created by bearden-tellez on 9/12/16.
  */
 public class WebChatClient {
-//    public String sendMessage(String messageToSend){
-//
-//
-//        BufferedReader in = new BufferedReader();
-//        String messageReceived =
-//
-//        return messageReceived;
-//    }
 
+    String serverResponse;
+
+    public String SendMessage(String message) {
+        try {
+            Socket clientSocket = new java.net.Socket("localhost", 8080);
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            System.out.println("output stream initialized");
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("input stream initialized");
+
+            System.out.println(message);
+
+            out.println(message);
+
+//            System.out.println("Can we make it here?");
+
+            serverResponse = in.readLine();
+
+
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+
+        return serverResponse;
+    }
 
 }
